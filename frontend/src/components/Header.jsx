c9cc7cbc61196c3da78a9761c0a2b690377929c1
Leonardo Monteiro <leonardoms17@hotmail.com>
2026-04-15 18:35:46 -0300

    feat: alterando href das nav

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,6 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { BsBag } from "react-icons/bs";
 import { BsTags } from "react-icons/bs";
-// import { BsCart2 } from "react-icons/bs";
 import Button from 'react-bootstrap/Button';
 export default function Header(){
     return(
@@ -11,8 +10,8 @@ export default function Header(){
         <Container>
           <Navbar.Brand className="btn btn-outline-light" href="#home">Joab Kids</Navbar.Brand>
           <Nav className="me-auto">
-            <Button className="me-2" variant="outline-light" href="#home"><BsBag/>Página Inicial</Button>
-            <Button variant="outline-light" href="#features"><BsTags/>Produtos</Button>
+            <Button className="me-2" variant="outline-light" href="#paginainicial"><BsBag/>Página Inicial</Button>
+            <Button variant="outline-light" href="#produtos"><BsTags/>Produtos</Button>
           </Nav>
         </Container>
       </Navbar>

08e8386d16c2d0a10d60a2c77b65b9499844200e
Leonardo Monteiro <leonardoms17@hotmail.com>
2026-04-15 16:10:57 -0300

    feat: codigos no header e adicionando na page home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+  import Home from './pages/Home.jsx';
 
 function App() {
   
 
   return (
     <>
-      
+      <Home/>
     </>
   )
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { BsBag } from "react-icons/bs";
+import { BsTags } from "react-icons/bs";
+import { BsCart2 } from "react-icons/bs";
+import Button from 'react-bootstrap/Button';
+export default function Header(){
+    return(
+        <Navbar variant="dark" style={{backgroundColor:"#2a9d8f"}}>
+        <Container>
+          <Navbar.Brand className="btn btn-outline-light" href="#home">Joab Kids</Navbar.Brand>
+          <Nav className="me-auto">
+            <Button className="me-2" variant="outline-light" href="#home"><BsBag/>Página Inicial</Button>
+            <Button variant="outline-light" href="#features"><BsTags/>Produtos</Button>
+          </Nav>
+        </Container>
+      </Navbar>
+  );
+}

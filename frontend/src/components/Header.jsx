f72a63f86e21d7729d5615a47aec656d18799048
Leonardo Monteiro <leonardoms17@hotmail.com>
2026-04-16 18:02:21 -0300

    feat e style no navbrand

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,11 +5,12 @@ import Navbar from 'react-bootstrap/Navbar';
 import { BsBag, BsHouseDoor, BsGrid, BsPersonPlus, BsBoxArrowInRight} from "react-icons/bs";
 import Button from 'react-bootstrap/Button';
 import tag from '../assets/JoabKidsTag.jpeg';
+import '../css/Nav.css';
 export default function Header(){
     return(
         <Navbar variant="dark" className="justify-content-center"style={{backgroundColor:"#003235"}}>
         <Container>
-          <Navbar.Brand href="/"><img src={tag} alt = "Joab Kids" height="100" /></Navbar.Brand>
+          <Navbar.Brand className ="navbrand" href="/"><img src={tag} alt = "Joab Kids" height="100" />Joab Kids - Shop</Navbar.Brand>
           <Nav className="mx-auto">
             <Button className="me-2" variant="outline-light" href="/">< BsHouseDoor className="me-1" />Página Inicial</Button>
             <Button className="me-2" variant="outline-light" href="/catalogo"><BsGrid className="me-1"/>Catálogo</Button>

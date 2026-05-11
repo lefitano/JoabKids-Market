652b510fca92eaf93f0134e0acf89e2ad3bc86ed
Saulo Ribeiro <sauloo.ribeirooo@gmail.com>
2026-05-11 14:56:11 -0300

    registrando rotas de usuarios e pedidos

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import produtosRoutes from "./produtos.routes.js";
+import usuariosRoutes from "./usuarios.routes.js";
+import pedidosRoutes from "./pedidos.routes.js";
 
 const router = Router();
 
 router.use("/produtos", produtosRoutes);
+router.use("/usuarios", usuariosRoutes);
+router.use("/pedidos", pedidosRoutes);
 
 export default router;

eb867d6be0ac8f21373e777f7fee3c9cbf505c32
Leonardo Monteiro <leonardoms17@hotmail.com>
2026-05-14 16:13:26 -0300

    adicionando Cadastro.jsx

--- frontend/src/pages/Cadastro.jsx
+++ frontend/src/pages/Cadastro.jsx
@@ -18,6 +18,7 @@ export default function Cadastro(){
     const[numero, setNumero] = useState('');
     const[numeroTelefone, setNumeroTel] = useState('');
     const[erroCadastro, setErroCadastro] = useState('');
+    const navigate = useNavigate();
 
     const handleRegister = async(e) => {
         e.preventDefault();
@@ -27,7 +28,7 @@ export default function Cadastro(){
                 setErroCadastro("Para realizar o cadastro, todos os campos devem ser preenchidos!");
                 return;
             }
-            navigate('/');
+            navigate('/login');
     }
 
 
@@ -38,42 +39,62 @@ export default function Cadastro(){
                 <h2 className="TituloCadastro">Cadastre-se agora!</h2>
 
                 <Form onSubmit={handleRegister}>
+                    <Row className="g-3">
+                        <Col xs={12} sm={10}>
                     <Form.Group className="mb-3" controlId="nomeCadastro">
                         <Form.Label>Nome completo</Form.Label>
                         <Form.Control type="text" value={nome} onChange={(e) => setNome(e.target.value)} placeholder="Digite seu nome completo"></Form.Control>
                     </Form.Group>
+                        </Col>
+                        <Col xs={12} sm={10}>
                     <Form.Group className="mb-3" controlId='FormEmailCadastro'>
                         <Form.Label>Email para cadastro</Form.Label>
                         <Form.Control type="email" value={emailCadastro} onChange={(e) => setEmailCadastro(e.target.value)} placeholder="Email de endereço válido"></Form.Control>
                     </Form.Group>
+                    </Col>
+                    <Col xs={12} sm={8}>
                     <Form.Group className="mb-3" controlId='FormSenhaCadastro'>
                         <Form.Label>Senha para conta</Form.Label>
                         <Form.Control type="password" value={senhaCadastro} onChange={(e) => setSenhaCadastro(e.target.value)} placeholder="Senha para cadastro"></Form.Control>
                     </Form.Group>
+                    </Col>
+                    <Col xs={12} sm={3}>
                     <Form.Group className="mb-3" controlId='cep'>
                         <Form.Label>Digite seu cep</Form.Label>
                         <Form.Control type="text" value={cep} onChange={(e)=> setCep(e.target.value)} placeholder="CEP"></Form.Control>
                     </Form.Group>
+                    </Col>
+                    <Col xs={12} sm={8}>
                     <Form.Group className="mb-3" controlId='cidade'>
                         <Form.Label>Sua cidade</Form.Label>
                         <Form.Control type="text" value={cidade} onChange={(e)=> setCidade(e.target.value)} placeholder="Nome da sua cidade"></Form.Control>
                     </Form.Group>
+                    </Col>
+                    <Col xs={12} sm={12}>
                     <Form.Group className="mb-3" controlId='rua'>
                         <Form.Label>Digite sua rua</Form.Label>
                         <Form.Control type="text" value={rua} onChange={(e)=> setRua(e.target.value)} placeholder="Nome da sua rua"></Form.Control>
                     </Form.Group>
+                    </Col>
+                    <Col xs={12} sm={8}>
                     <Form.Group className="mb-3" controlId='bairro'>
                         <Form.Label>Digite seu bairro</Form.Label>
                         <Form.Control type="text" value={bairro} onChange={(e)=> setBairro(e.target.value)} placeholder="Nome do seu bairro"></Form.Control>
                     </Form.Group>
+                    </Col>
+                    <Col xs={12} sm={3}>
                     <Form.Group className="mb-3" controlId='numero'>
-                        <Form.Label>Digite o número</Form.Label>
-                        <Form.Control type="text" value={numero} onChange={(e)=> setNumero(e.target.value)} placeholder="Numero da sua residência"></Form.Control>
+                        <Form.Label>Nº</Form.Label>
+                        <Form.Control type="text" value={numero} onChange={(e)=> setNumero(e.target.value)} placeholder="Número"></Form.Control>
                     </Form.Group>
+                    </Col>
+                    <Col xs={12} sm={4}>
                     <Form.Group className="mb-3" controlId='numeroTelefone'>
-                        <Form.Label>Digite seu número de celular com DDD</Form.Label>
-                        <Form.Control type="tel" value={numeroTelefone} onChange={(e)=> setNumeroTel(e.target.value)} placeholder="Número para contato"></Form.Control>
-                    </Form.Group> 
+                        <Form.Label>Celular para contato com DDD</Form.Label>
+                        <Form.Control type="tel" value={numeroTelefone} onChange={(e)=> setNumeroTel(e.target.value)} placeholder="(DDD) + Número"></Form.Control>
+                    </Form.Group>
+                    </Col>
+                    </Row> 
                     {erroCadastro && <p className="ErroFormCadastro">{erroCadastro}</p>}
                     <Button variant="primary" type="submit">
                         Cadastrar
